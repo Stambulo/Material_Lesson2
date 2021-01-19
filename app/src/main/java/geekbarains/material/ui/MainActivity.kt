341606615b83eabf20e8cbe27dce983db8c30498
Stambulo Anatolii <stambulo@yandex.ru>
2021-01-19 21:26:12 +0200

    Lesson2_is_done

--- app/src/main/java/geekbarains/material/ui/MainActivity.kt
+++ app/src/main/java/geekbarains/material/ui/MainActivity.kt
@@ -1,14 +1,34 @@
 package geekbarains.material.ui
 
+import android.content.SharedPreferences
+import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import geekbarains.material.R
 import geekbarains.material.ui.picture.PictureOfTheDayFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var pref: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        pref = this.getSharedPreferences("theme", MODE_PRIVATE)
+
+
+        if (pref.getString("theme", "Light Theme") == "Light Theme"){
+            setTheme(R.style.AppTheme)
+        }
+
+        if (pref.getString("theme", "Light Theme") == "Dark Theme"){
+            setTheme(R.style.MyDarkAppTheme)
+        }
+
+        if (pref.getString("theme", "Light Theme") == "Custom Theme"){
+            setTheme(R.style.AppTheme_Orange)
+        }
+
+
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
--- app/src/main/java/geekbarains/material/ui/themes/ThemesFragment.kt
+++ app/src/main/java/geekbarains/material/ui/themes/ThemesFragment.kt
@@ -1,17 +1,23 @@
 package geekbarains.material.ui.themes
 
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
 import com.google.android.material.chip.Chip
 import geekbarains.material.R
 import kotlinx.android.synthetic.main.fragment_themes.*
 
 class ThemesFragment : Fragment() {
+    private lateinit var sharedPref: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,13 +28,37 @@ class ThemesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         chipGroup.setOnCheckedChangeListener { chipGroup, position ->
 
-            Log.i("--->", "onViewCreated: ")
             chipGroup.findViewById<Chip>(position)?.let {
-                Toast.makeText(context, "Выбран ${it.text}", Toast.LENGTH_SHORT).show()
+                when (it.text){
+                    "Light Theme" -> {
+                        Toast.makeText(context, "Выбран ${it.text}", Toast.LENGTH_SHORT).show()
+                        sharedPref.edit().putString("theme", "Light Theme").apply()
+                        activity?.recreate()
+                    }
+
+                    "Dark Theme" -> {
+                        Toast.makeText(context, "Выбран ${it.text}", Toast.LENGTH_SHORT).show()
+                        sharedPref.edit().putString("theme", "Dark Theme").apply()
+                        activity?.recreate()
+                    }
+
+                    "Custom Theme" -> {
+                        Toast.makeText(context, "Выбран ${it.text}", Toast.LENGTH_SHORT).show()
+                        sharedPref.edit().putString("theme", "Custom Theme").apply()
+                        activity?.recreate()
+                    }
+
+                    else -> Toast.makeText(context, "Else case $position, ${it.text}",
+                        Toast.LENGTH_SHORT).show()
+                }
             }
         }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        sharedPref = context.getSharedPreferences("theme", 0)
     }
 }
